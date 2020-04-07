46d06c3cbace8d278ff953fe396c1b1362bd0ef3
Ashcon27 <60788388+Ashcon27@users.noreply.github.com>
2020-04-06 23:59:50 -0400

    !create has been added and title

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 import discord
 from discord.ext import commands
 
+
 #load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -16,7 +17,16 @@ async def on_ready():
 @bot.command(name="hi", help="Says hello")
 async def say_hello(ctx):
     await ctx.send(f"Hello {ctx.author.display_name}")
-
+   
+@bot.command(name="create", help="basic info for poll")
+async def creator(ctx):
+    await ctx.send(f"Hi{ctx.author.display_name}. Let's create a poll! Type .title")
+          
+@client.event
+async def on_message(message):
+    if message.content.startswith(".title"):
+        await client.send_message(message.channel, message.content[6:])
+        
 bot.run(TOKEN)
 
 
